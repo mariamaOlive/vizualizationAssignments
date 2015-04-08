2d5d9a86f256f24da9ab20c10c3557af0745eafd
tereseno <terese_n@hotmail.com>
2015-04-08 16:07:31 +0200

    Task2 files

---
+++ Task2.cpp
@@ -0,0 +1,149 @@
+//---------------------------------------------// 
+#include "stdafx.h"                             
+//---------------------------------------------//
+#include "Task2.h"                
+//---------------------------------------------// 
+#include "Properties.h"      
+#include "GLGeometryViewer.h"
+#include "GeoXOutput.h"                         
+//---------------------------------------------//
+                                             
+                                          
+IMPLEMENT_GEOX_CLASS( Task2, 0 )    
+{                                               
+	BEGIN_CLASS_INIT( Task2 );       
+
+	//input values slope and number of points
+	ADD_SEPARATOR("Negative slope")
+	ADD_FLOAT32_PROP(slope, 0)
+	ADD_INT32_PROP(numberPoints,0)
+
+	//input circle values
+	ADD_SEPARATOR("Circle")
+	ADD_FLOAT32_PROP(Radius, 0)
+	ADD_VECTOR2F_PROP(Center, 0)
+	ADD_INT32_PROP(NumSamples, 0)
+
+	ADD_NOARGS_METHOD(Task2::NegativeSlope)   
+	ADD_NOARGS_METHOD(Task2::DrawCircle)
+}                                               
+
+QWidget* Task2::createViewer()
+{
+    viewer = new GLGeometryViewer();
+    return viewer;
+}
+
+Task2::Task2()          
+{          
+	viewer = NULL;
+
+	//scatterplot axis
+	scatterOrigin = makeVector2f(-1.5,0.0);
+	endAxisX = makeVector2f(-0.5,0.0);
+	endAxisY = makeVector2f(-1.5,1.0);
+
+	//parallel cord axis
+	startX1 = makeVector2f(0.5,0.0);
+	endX1 = makeVector2f(0.5,1.0);
+	startX2 = makeVector2f(1.5,0.0);
+	endX2 = makeVector2f(1.5,1.0);
+
+	//slope values
+	slope=-1;
+	numberPoints=10;
+
+	//circle values
+	Radius = 0.1;
+	Center = makeVector2f((endAxisX[0]+scatterOrigin[0])/2,(endAxisY[1]+scatterOrigin[1])/2);
+	NumSamples = 20;
+}                                               
+                                              
+Task2::~Task2()        
+{             
+}                                               
+                                               
+void Task2::CreateAxis()        
+{                        
+	Vector4f color = makeVector4f(1,1,1,1);
+	//scatter plot axes
+	viewer->addLine(scatterOrigin, endAxisX, color, 2);
+	viewer->addLine(scatterOrigin, endAxisY, color, 2);
+
+	//parallel coordinates plot "axes"
+	viewer->addLine(startX1, endX1, color, 2);
+	viewer->addLine(startX2, endX2, color, 2);
+}    
+
+void Task2::CreateParaline(float xvalue, float yvalue){
+	//make parallel line between points
+	// X1 = XparStart, pointXvalue
+	// Y1 = YparStart, pointYvalue
+
+	Vector2f X1 = makeVector2f(startX1[0],(xvalue-scatterOrigin[0]));
+	Vector2f Y1 = makeVector2f(startX2[0],yvalue);
+
+	viewer->addLine( X1, Y1);
+}
+
+void Task2::NegativeSlope()
+{
+	viewer->clear();
+	CreateAxis();
+
+	float pointX= scatterOrigin[0];
+	float pointY;
+
+	Vector4f color=makeVector4f(1,0,0,1);
+	int size= 5;
+	
+	float pace=(endAxisX[0]-scatterOrigin[0])/numberPoints;
+
+	for(int i=0; i<numberPoints; i++){
+		pointX+=pace;
+		pointY= slope*pointX;
+		Vector2f scatterPoint= makeVector2f(pointX,pointY);
+		viewer->addPoint(scatterPoint);
+
+		CreateParaline(pointX, pointY);
+	}
+	
+	/*float pointY= endAxisY[1];
+	Vector4f color=makeVector4f(1,0,0,1);
+	int size= 5;
+	
+	for(int i=0; i<10; i++){
+		pointX+=.1;
+		pointY-=.1;
+		Vector2f scatterPoint= makeVector2f(pointX,pointY);
+		viewer->addPoint(scatterPoint);
+
+		CreateParaline(pointX,pointY);
+	}*/
+
+	// display changes
+    viewer->refresh();
+}
+
+void Task2::DrawCircle(){
+	viewer->clear();
+	CreateAxis();
+	
+	//Create a circle by iterating over 360 degrees.
+	for(int i=1; i < NumSamples; i++){
+
+		float pointX = Center[0] + Radius * cos(2 * 3.14159265 * float(i)/float(NumSamples-1));
+		float pointY = Center[1] + Radius * sin(2 * 3.14159265 * float(i)/float(NumSamples-1));
+		//radiens not degrees
+		const Vector2f A = makeVector2f(pointX,pointY);
+		
+		CreateParaline(pointX,pointY);	
+
+		viewer->addPoint(A);  
+	}
+
+	// display changes
+    viewer->refresh();
+
+}
+
---
+++ Task2.h
@@ -0,0 +1,58 @@
+//-------------------------------------------- // 
+#ifndef Task2H                    
+#define Task2H                     
+//-------------------------------------------- //
+#include "Experiment.h"                        
+#include "LinearAlgebra.h"             
+#include "GLGeometryViewer.h"
+//-------------------------------------------- //
+                                            
+
+///        First own experiment               //
+
+class Task2 : public Experiment    
+{                                              
+    GEOX_CLASS(Task2)              
+                                              
+//Constructor / Destructor                     
+public:                                       
+    Task2();                       
+    virtual ~Task2();              
+                                             
+//Methods                                      
+public:                                       
+    void CreateAxis();    
+	void NegativeSlope();
+	void CreateParaline(float x, float y);
+	void DrawCircle();
+
+	virtual QWidget* createViewer();
+                                               
+//Attributes       
+private:
+	//scatterplot axis
+	Vector2f scatterOrigin;
+	Vector2f endAxisX;
+	Vector2f endAxisY;
+
+	//parrallel plot axis
+	Vector2f startX1;
+	Vector2f endX1;
+	Vector2f startX2;
+	Vector2f endX2;
+
+	//Parameters for slope
+	float slope;
+	int numberPoints;
+
+	//Parametrs for the circle
+	float Radius;
+	Vector2f Center;
+	int NumSamples;
+
+protected:
+    GLGeometryViewer* viewer;
+
+};                                            
+                                             
+#endif                                         
