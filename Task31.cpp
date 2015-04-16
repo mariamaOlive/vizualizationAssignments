5e8a1d1429eaba125124d02c62da5c1ef0766fde
tereseno <terese_n@hotmail.com>
2015-04-16 15:07:17 +0200

    removing namespace

--- Task31.cpp
+++ Task31.cpp
@@ -113,24 +113,3 @@ void Task31::DrawScalarField()
 }
 
 
-
-
-namespace
-{
-    ///Returns the next power-of-two
-    int32 NextPOT(int32 n)
-    {
-        n--;
-        n |= n >> 1;   // Divide by 2^k for consecutive doublings of k up to 32,
-        n |= n >> 2;   // and then or the results.
-        n |= n >> 4;
-        n |= n >> 8;
-        n |= n >> 16;
-        n++;           // The result is a number of 1 bits equal to the number
-                       // of bits in the original number, plus 1. That's the
-                       // next highest power of 2.
-        return n;
-    }
-}
-
-
