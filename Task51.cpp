e391b2d5d1fa54e671720e359e5f131e9a11ba77
tereseno <terese_n@hotmail.com>
2015-04-30 22:27:44 +0200

    Skeletton for lab5 part1

---
+++ Task51.cpp
@@ -0,0 +1,104 @@
+//---------------------------------------------------------------------------
+#include "stdafx.h"
+//---------------------------------------------------------------------------
+#include "Task51.h"
+//---------------------------------------------------------------------------
+#include "Properties.h"
+#include "GLGeometryViewer.h"
+#include "GeoXOutput.h"
+//---------------------------------------------------------------------------
+
+#include <limits>
+#include "Field2.hpp"
+
+IMPLEMENT_GEOX_CLASS( Task51, 0)
+{
+    BEGIN_CLASS_INIT( Task51 );
+
+    ADD_SEPARATOR("Start Values")
+    ADD_STRING_PROP(VectorfieldFilename, 0)
+	ADD_FLOAT32_PROP(XStart, 0)
+	ADD_FLOAT32_PROP(YStart, 0)
+	ADD_FLOAT32_PROP(MaxDistance, 0)
+    ADD_FLOAT32_PROP(ArrowScale, 0)
+
+	ADD_SEPARATOR("Euler")	
+	ADD_FLOAT32_PROP(EulerStepSize,0)
+	ADD_INT32_PROP(EulerSteps,0)
+
+	ADD_SEPARATOR("RungeKutta")
+	ADD_FLOAT32_PROP(RKStepSize,0)
+	ADD_INT32_PROP(RKSteps,0)  
+
+
+    ADD_NOARGS_METHOD(Task51::DrawVectorField)
+	ADD_NOARGS_METHOD(Task51::EulerStreamlines)
+	ADD_NOARGS_METHOD(Task51::RungeKuttaStreamlines)
+}
+
+QWidget* Task51::createViewer()
+{
+    viewer = new GLGeometryViewer();
+    return viewer;
+}
+
+Task51::Task51()
+{
+    viewer = NULL;
+    VectorfieldFilename = "Cylinderclose2CT10.am";
+    ArrowScale = 0.1;
+
+	//start values
+	XStart = 1;
+	YStart = 0;
+	MaxDistance = 5.3;
+
+	//Euler values
+	EulerStepSize = 0.1;
+	EulerSteps = 100;
+
+	//Runge-Kutta values
+	RKStepSize = 0.3;
+	RKSteps = 30;
+}
+
+Task51::~Task51() {}
+
+//From ExpampleExperimentField (remove later)
+void Task51::DrawVectorField()
+{
+    viewer->clear();
+
+    //Load the vector field
+    VectorField2 field;
+    if (!field.load(VectorfieldFilename))
+    {
+        output << "Error loading field file " << VectorfieldFilename << "\n";
+        return;
+    }
+
+    //Draw vector directions (constant length)
+    for(float32 x=field.boundMin()[0]; x<=field.boundMax()[0]; x+=0.2)
+    {
+        for(float32 y=field.boundMin()[1]; y<=field.boundMax()[1]; y+=0.2)
+        {
+            Vector2f vec = field.sample(x,y);
+            vec.normalize();
+
+            viewer->addLine(x, y, x + ArrowScale*vec[0], y + ArrowScale*vec[1]);
+        }
+    }
+
+    viewer->refresh();
+}
+
+//Euler
+void Task51::EulerStreamlines(){
+
+}
+
+
+//Runge-Kutta
+void Task51::RungeKuttaStreamlines(){
+
+}
---
+++ Task51.h
@@ -0,0 +1,63 @@
+//---------------------------------------------------------------------------
+#ifndef Task51H
+#define Task51H
+//---------------------------------------------------------------------------
+#include "Experiment.h"
+#include "LinearAlgebra.h"
+#include "GLGeometryViewer.h"
+//---------------------------------------------------------------------------
+
+/// This is an example experiment.
+///
+/// The code is meant to demonstrate how
+///  to use the GeoX framework
+///
+
+class Task51 : public Experiment
+{
+    GEOX_CLASS(Task51)
+
+//Constructor / Destructor
+public:
+    Task51();
+    virtual ~Task51();
+
+//Methods
+public:
+    void DrawVectorField();	//drawVectorField to be removed later
+    virtual QWidget* createViewer();
+	
+	//Euler
+	void EulerStreamlines();
+
+	//RungeKutta
+	void RungeKuttaStreamlines();
+
+//Attributes
+public:
+
+    ///File name of the vector field
+    string VectorfieldFilename;
+
+	//start values
+	float XStart;
+	float YStart;
+	float MaxDistance;
+
+    ///Length of the arrows		TO BE REMOVED
+    float ArrowScale;
+
+	//Euler values
+	float EulerStepSize;
+	int EulerSteps;
+
+	//Runge-Kutta values
+	float RKStepSize;
+	int RKSteps;
+
+
+protected:
+    GLGeometryViewer* viewer;
+};
+
+#endif
