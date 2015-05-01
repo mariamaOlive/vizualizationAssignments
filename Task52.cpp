84cf324ebf55b74ed7eda6b4f82009c953d7c1cf
tereseno <terese_n@hotmail.com>
2015-05-01 17:09:16 +0200

    adding your stuff to mine

--- Task52.cpp
+++ Task52.cpp
@@ -228,7 +228,7 @@ void Task52::GridSeeding(){
 	float ymin = field.boundMin()[1];
 	float ymax = field.boundMax()[1];
 
-	int points = n / (GridPointsX + GridPointsY);
+	int points = n / (GridPointsX + GridPoitnsY);
 
 }
 
@@ -240,45 +240,71 @@ void Task52::RungeKuttaStreamlines(VectorField2 field, float startX, float start
 	
 	//Defining the start point
 	Vector2f startPoint = makeVector2f(startX,startY);
-	viewer->addPoint(startPoint);
+	//Vector2f startPoint = makeVector2f((field.boundMin()[0] + field.boundMax()[0])/2,(field.boundMin()[1] + field.boundMax()[1])/2); // starting in the middle
+	//Vector2f startPoint = makeVector2f(field.boundMin()[0], field.boundMin()[1]);
 
 	Vector2f x = makeVector2f(startPoint[0], startPoint[1]);
 
-	RKStepSize = backwards ? -RKStepSize : RKStepSize; // if "backwards" is checked, inverts sign of integration step
-	float a, b, length;
-	float accLength = 0;
+	float RKStepSize2 = backwards ? -RKStepSize : RKStepSize; // if "backwards" is checked, inverts sign of integration step
+	float a, b, length, speed;
+	float arcLength = 0;
+	bool outOfBounds = false;
+	bool tooSlow = false;
 
-	for(int i = 0; i < RKSteps; i++){
+	//Checks if initial point is out of boundaries
+	if ((x[0] < field.boundMin()[0])||(x[0] > field.boundMax()[0])||(x[1] < field.boundMin()[1])||(x[1] > field.boundMax()[1])) {
+		outOfBounds = true;
+		output << "Out of bounds! \n";
+	}
+
+	for(int i = 0; ((i < RKSteps) && (arcLength < MaxLength) && (!outOfBounds) && (!tooSlow)); i++){
 
 	//The 4 vectors of th RK method
 		Vector2f v1 = field.sample(x[0],x[1]);
 		v1.normalize();
 		
-		Vector2f v2p = makeVector2f((x[0]+RKStepSize*v1[0]/2),(x[1]+RKStepSize*v1[1]/2));
+		Vector2f v2p = makeVector2f((x[0]+RKStepSize2*v1[0]/2),(x[1]+RKStepSize2*v1[1]/2));
 		Vector2f v2 = field.sample(v2p[0],v2p[1]);
 		v2.normalize();
 
-		Vector2f v3p = makeVector2f((x[0]+RKStepSize*v2[0]/2),(x[1]+RKStepSize*v2[1]/2));
+		Vector2f v3p = makeVector2f((x[0]+RKStepSize2*v2[0]/2),(x[1]+RKStepSize2*v2[1]/2));
 		Vector2f v3 = field.sample(v3p[0],v3p[1]);
 		v3.normalize();
 
-		Vector2f v4p = makeVector2f((x[0]+RKStepSize*v3[0]),(x[1]+RKStepSize*v3[1]));
+		Vector2f v4p = makeVector2f((x[0]+RKStepSize2*v3[0]),(x[1]+RKStepSize2*v3[1]));
 		Vector2f v4 = field.sample(v4p[0],v4p[1]);
 		v4.normalize();
 
 	//Combine the 4 vectors to get the end position
-		Vector2f x1 = makeVector2f(x[0]+RKStepSize*(v1[0]/6 + v2[0]/3 + v3[0]/3 + v4[0]/6), x[1]+RKStepSize*(v1[1]/6 + v2[1]/3 + v3[1]/3 + v4[1]/6));
+		Vector2f x1 = makeVector2f(x[0]+RKStepSize2*(v1[0]/6 + v2[0]/3 + v3[0]/3 + v4[0]/6), x[1]+RKStepSize2*(v1[1]/6 + v2[1]/3 + v3[1]/3 + v4[1]/6));
 
-		//viewer->addPoint(x);
-        viewer->addLine(x, x1, RKcolor, 2);
+	//Calculates arc length
 		a = x1[0]-x[0];
 		b = x1[1]-x[1];
 		length = sqrt(pow(a,2)+pow(b,2));
-		accLength += length;
-		//output << "length: " << length << " , total: " << accLength << "\n";
+		arcLength += length;
 
-
-		x = x1;
+	//Calculates speed (actual vector size) by sampling
+		Vector2f sampleVector = field.sample(x1[0], x1[1]);
+		a = sampleVector[0]-x1[0];
+		b = sampleVector[1]-x1[1];
+		speed = sqrt(pow(a,2)+pow(b,2));
+		output << "speed: " << speed << "\n";
+		
+		//Checks boundary limits
+		if ((x1[0] < field.boundMin()[0])||(x1[0] > field.boundMax()[0])||(x1[1] < field.boundMin()[1])||(x1[1] > field.boundMax()[1])) {
+			outOfBounds = true;
+			output << "Out of bounds! \n";
+		}
+		else if (speed < MinSpeed) {
+			tooSlow = true;
+			output << "Vector speed too slow... \n";
+		}
+		else {
+			viewer->addPoint(x);
+			viewer->addLine(x, x1, RKcolor, 2);	
+			x = x1;
+		}
 	}
 	viewer->refresh();
 }
