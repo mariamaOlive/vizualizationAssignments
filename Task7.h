5b0ba3d6e0b2d5afccdd3ca39672f7f7df7abba7
tereseno <terese_n@hotmail.com>
2015-05-14 10:30:54 +0200

    adding new files for last assignment

---
+++ Task7.h
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------------
+#ifndef Task7H
+#define Task7H
+//---------------------------------------------------------------------------
+#include "Experiment.h"
+#include "LinearAlgebra.h"
+#include "GLGeometryViewer.h"
+//---------------------------------------------------------------------------
+
+
+/// This is an example experiment.
+///
+/// The code is meant to demonstrate how
+///  to use the GeoX framework
+///
+class Task7 : public Experiment
+{
+    GEOX_CLASS(Task7)
+
+//Constructor / Destructor
+public:
+    Task7();
+    virtual ~Task7();
+
+//Methods
+public:
+    void DrawScalarField();
+    void DrawVectorField();
+    void DrawTexture();
+    virtual QWidget* createViewer();
+
+//Attributes
+public:
+    ///File name of the scalar field
+    string ScalarfieldFilename;
+
+    ///File name of the vector field
+    string VectorfieldFilename;
+
+    ///Length of the arrows
+    float ArrowScale;
+
+    ///File name of the image for the texture
+    string ImageFilename;
+
+    ///Whether to draw the texture in RGB or grayscale
+    bool bColoredTexture;
+
+protected:
+    GLGeometryViewer* viewer;
+};
+
+
+#endif
